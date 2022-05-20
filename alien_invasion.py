277d3af1e9b847e0fcccdf1696caed87212b134d
Brady Hammond <brady_hammond1.baylor.edu>
2022-05-20 13:22:55 -0500

    added ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,2 +1,32 @@
+import sys
 import pygame
-print(pygame.version)
+from settings import Settings
+from ship import Ship
+
+def run_game():
+    # Initialize pygame, settings and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship
+    ship = Ship(screen)
+
+    # Start main loop for the game
+    while True:
+
+        # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+        
+        # Make the most recently drawn screen visible
+        pygame.display.flip()
+
+run_game()
